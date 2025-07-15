8a0bc4f387c11e2e5ceea701a742f531f7879af7
Lubna Febin <lubnafebin13@gmail.com>
2025-07-15 21:18:08 +0530

    feat: add new images for Home component and implement scroll effect

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,4 +1,15 @@
+import { useEffect, useState } from "react";
 export const Home = () => {
+  const [scrollY, setScrollY] = useState(0);
+
+  const handleScroll = () => {
+    setScrollY(window.scrollY);
+  };
+
+  useEffect(() => {
+    window.addEventListener("scroll", handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, []);
   return (
     <>
       <div className="relative w-full h-screen overflow-hidden">
@@ -19,29 +30,34 @@ export const Home = () => {
             </li>
           </ul>
         </nav>
-        {/* <img
-          src="/assets/sky.PNG"
-          alt=""
-          className="absolute top-0 left-0 w-full h-[50%] object-cover z-10"
-        /> */}
         <img
-          src="/assets/wayanad.jpg"
+          src="/assets/ghibli_hill_front.png"
           alt=""
-          className="absolute top-0 left-0 w-full h-full object-cover z-20"
+          className="absolute top-0 left-0 w-full h-[65%] object-cover z-10"
+          style={{
+            transform: `scale(${1 + scrollY * 0.0007})`,
+            transformOrigin: "bottom center",
+          }}
         />
-        {/* <img
-          src="/assets/mountain-countout.JPG"
+        <img
+          src="/assets/ghibli_hill.png"
           alt=""
-          className="absolute bottom-0 left-0 w-full h-[80%] object-cover z-10"
-        /> */}
-        <div className="relative px-10 z-20 mt-60">
+          className="absolute bottom-0 left-0 w-full h-[35%] object-cover z-20"
+        />
+        <div
+          className="absolute top-[40%] left-1/2 transform -translate-x-1/2 text-center text-white px-6 z-20 w-full max-w-4xl"
+          style={{
+            transform: `translate(-50%, ${-scrollY * 0.3}px)`,
+            opacity: 1 - scrollY * 0.002,
+          }}
+        >
           <h1 className="font-bold text-5xl text-white ">
             Explore the beauty of Wayanad
           </h1>
           <p className="text-xl mt-5 font-serif text-white">
             discover nature,culture & calm in every every corner
           </p>
-          <button className="bg-white text-green rounded-lg mt-5 pl-5 pr-5 p-1 ml-40">
+          <button className="mt-6 px-4 py-1 rounded-full bg-white text-green-900 font-semibold text-lg shadow-lg hover:bg-green-100 transition duration-300">
             Explore
           </button>
         </div>
