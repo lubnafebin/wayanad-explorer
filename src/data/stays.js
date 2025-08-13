faa0a40f93f8cebf36dc1da40dfa924dcbe533fe
Lubna Febin <lubnafebin13@gmail.com>
2025-08-13 14:50:28 +0530

    feat: update STAYS data with new images.

--- src/data/stays.js
+++ src/data/stays.js
@@ -1,75 +1,74 @@
 export const STAYS = [
-   {
-    id: 1,
-    name: "Grassroots Wayanad",
-    type: "glamping",
-    location: "Kalpetta, Wayanad",
-    price: "from ₹5,000",
-    image: "/assets/stays/glamping.webp",
-    rating:"2"
-  },
   {
-    id: 2,
+    id: 1,
     name: "Banasura Hill Resort",
     type: "resort",
     location: "Vellamunda, Wayanad",
     price: "from ₹8,000",
     image: "/assets/stays/resort1.webp",
-    rating:"4"
+    rating: "4",
   },
   {
-    id: 3,
+    id: 2,
     name: "Tranquil Villa",
     type: "villa",
     location: "Kolagappara, Wayanad",
     price: "from ₹6,000",
     image: "/assets/stays/villa1.webp",
-    rating:"3"
+    rating: "4",
   },
   {
-    id: 4,
+    id: 3,
     name: "Petals Glamping",
     type: "glamping",
     location: "Vythiri, Wayanad",
     price: "from ₹6,500",
     image: "/assets/stays/grt-hotel.webp",
-    rating:"5"
+    rating: "5",
   },
   {
-    id: 5,
+    id: 4,
     name: "Enteveedu Homestay",
     type: "homestay",
     location: "Panamaram, Wayanad",
     price: "from ₹3,500",
-    image: "/assets/resort.jpg",
-    rating:""
+    image: "/assets/stays/homestay.webp",
+    rating: "4",
   },
   {
-    id: 6,
+    id: 5,
     name: "Vythiri Village Resort",
     type: "resort",
     location: "Vythiri, Wayanad",
     price: "form ₹6,347",
     image: "/assets/stays/resort2.webp",
-    rating:"5"
+    rating: "5",
   },
   {
-    id: 7,
+    id: 6,
     name: "Silverwoods Villa",
     type: "villa",
     location: "Kalpetta, Wayanad",
     price: "from ₹9,000",
-    image: "/assets/stays/resort.jpg",
-    rating:""
+    image: "/assets/stays/villa2.webp",
+    rating: "5",
   },
-   {
-    id: 8,
+  {
+    id: 7,
     name: "Aranyakam Homestay",
     type: "homestay",
     location: "Meppadi, Wayanad",
     price: "from ₹4,000",
-    image: "/assets/resort.jpg",
-    rating:""
+    image: "/assets/stays/homestay1.webp",
+    rating: "5",
   },
-
-]
+  {
+    id: 8,
+    name: "Grassroots Wayanad",
+    type: "glamping",
+    location: "Kalpetta, Wayanad",
+    price: "from ₹5,000",
+    image: "/assets/stays/glamping.webp",
+    rating: "5",
+  },
+];
