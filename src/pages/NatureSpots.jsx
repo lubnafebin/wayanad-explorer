2233fcc11f59f7eae951a3950ba3cf0190fdaef0
Lubna Febin <lubnafebin13@gmail.com>
2025-08-20 20:53:34 +0530

    feat: update background image for Nature Spots page to enhance visual appeal

--- src/pages/NatureSpots.jsx
+++ src/pages/NatureSpots.jsx
@@ -41,7 +41,7 @@ export const NatureSpots = () => {
         className="relative text-white py-24 text-center bg-cover"
         style={{
           backgroundImage:
-            "url('https://images.unsplash.com/photo-1421789665209-c9b2a435e3dc?q=80&w=1171&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D')",
+            "url('https://plus.unsplash.com/premium_photo-1673799490772-2ea0b711e43b?q=80&w=722&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D')",
         }}
       >
         <h1 className="text-4xl md:text-6xl font-bold">
